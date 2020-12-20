e7242b2256ce001b3d24cf53fe68701c57b6df15
Joshua Morales <joshuapmorales@gmail.com>
2020-12-20 01:19:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -19,7 +19,7 @@ end
 
 
 
-def introduction_with_language_optional
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
